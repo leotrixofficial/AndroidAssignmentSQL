75bfa81daa1df4900da74ac0e4052b7a0fbdabf1
leotrixofficial <leotrix3@gmail.com>
2017-11-22 18:39:29 +1100

    ListView for products

--- app/src/main/java/com/example/leotrix/androidassignmentsql/ProductActivity.java
+++ app/src/main/java/com/example/leotrix/androidassignmentsql/ProductActivity.java
@@ -1,9 +1,21 @@
 package com.example.leotrix.androidassignmentsql;
 
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class ProductActivity extends AppCompatActivity {
+    // Database and table names
+    protected String databaseName = "myDB",
+            loginTableName = "Login",
+            productsTableName = "Products";
+
+    // Database for Login and Products tables
+    protected SQLiteDatabase myDB;
+
+    // Products cursor for querying products data from database
+    protected Cursor productsCursor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
